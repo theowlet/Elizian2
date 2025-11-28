5c136d8aeb71e5fdf99cfe4819480dbfb4cd9572
Nishant Verma <mailfornishantverma@gmail.com>
2025-11-28 16:08:50 +0530

    Initial commit - Full ElizianAppExpo project

--- backend/db/migrations/2025-11-29-partner-dining-metadata.sql
+++
@@ -1,8 +0,0 @@
--- Add dietary preferences and average cost columns for dining partners
-ALTER TABLE partners
-    ADD COLUMN IF NOT EXISTS dietary_preferences TEXT[] DEFAULT '{}',
-    ADD COLUMN IF NOT EXISTS average_cost_for_two NUMERIC(10,2);
-
-COMMENT ON COLUMN partners.dietary_preferences IS 'Array of dietary preference tags (e.g., vegetarian, vegan)';
-COMMENT ON COLUMN partners.average_cost_for_two IS 'Average cost for two people in INR';
-
