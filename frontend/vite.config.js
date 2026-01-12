9e260780669be3ebb0fa050dbc33c58de9632663
kk202393 <kailash.lodhi@gmail.com>
2026-01-13 02:20:27 +0530

    update vite

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -10,6 +10,10 @@ export default defineConfig({
   server: {
     port: 8080,
     strictPort: true,
+    allowedHosts: [
+      'elizian.in',
+      'www.elizian.in'
+    ]
   },
   build: {
     outDir: 'dist',
